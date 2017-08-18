e271e8b44c44229f932244040a8384d7d9bd513b
cqlouis <liurui666666@hotmail.com>
2017-08-18 08:34:52 +0800

    递归法fibonacci数列

---
+++ fibonacci_resursion.py
@@ -0,0 +1,13 @@
+def fib(n):
+	if n < 0:
+		return print("Errous input")
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fib(n - 1) + fib(n - 2)
+
+
+n = int(input("请输入n: "))
+result = fib(n)
+print("fibonacci(%d) =  %d" % (n, result))
